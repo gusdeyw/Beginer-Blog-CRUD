4b654cfb6eaca582c4b4305231fb1acaa0355bf4
yudacoolz <111553945+yudacoolz@users.noreply.github.com>
2022-08-22 15:38:55 +0800

    Update update.php

--- update.php
+++ update.php
@@ -2,7 +2,55 @@
 include 'koneksi.php';
 
 $id = $_POST['id_blog'];
-// $gambar = $_POST['fileToUpload'];
+
+$target_dir = "img/";
+$target_file = $target_dir . rand(10,100).rand(10,100).rand(10,100).rand(10,100).rand(10,100).rand(10,100). basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
 $judul = $_POST['judul'];
 $deskripsi = $_POST['deskripsi'];
 $tanggal = $_POST['tanggal']; 
@@ -12,7 +60,7 @@ $status = $_POST['status'];
 
 // mysqli_query($con,$query);
 // entah kenapa pas aku nambahin SET gambar='$gambar' ini dia jadi ga mau nge proses edit nya gambar='$gambar'
-mysqli_query($con,"UPDATE list_gambar SET judul='$judul', deskripsi='$deskripsi', tanggal='$tanggal', status='$status' WHERE id_blog = '$id'");
+mysqli_query($con,"UPDATE list_gambar SET gambar='$target_file', judul='$judul', deskripsi='$deskripsi', tanggal='$tanggal', status='$status' WHERE id_blog = '$id'");
 
 echo $id;
 
